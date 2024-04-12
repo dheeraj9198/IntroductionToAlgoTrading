d4f07c4bf8f6300e9d70f3507630b06aaef9471a
dssachan <dssachan@amazon.com>
2024-04-11 17:25:49 -0700

    Advanced straddle

--- src/main/java/StraddleWithVwap.java
+++ src/main/java/StraddleWithVwap.java
@@ -22,10 +22,11 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class Serenity_2_0 {
+public class StraddleWithVwap {
 
     private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(1);
-    private static final AtomicInteger trades =new AtomicInteger(0);
+    private static final AtomicInteger trades = new AtomicInteger(0);
+
     @SneakyThrows
     public static void main(String[] strings) throws FileNotFoundException {
         Date start = new Date();
@@ -33,7 +34,7 @@ public class Serenity_2_0 {
         Map<Date, Float> DATE_PROFIT_MAP = new TreeMap<>();
         Map<Date, List<String>> STRING_LIST_MAP = new TreeMap<>();
 
-        PrintWriter printWriter = new PrintWriter(new File("FinalResults/backTestOutput/result-serenity-atm-2.0-" + "-" + new Date().getTime()));
+        PrintWriter printWriter = new PrintWriter(new File("FinalResults/backTestOutput/" +StraddleWithVwap.class.getName()+ "-" + new Date().getTime()));
         printWriter.flush();
         File fileo = new File("FinalResults/input/atm");
         File[] files = fileo.listFiles();
@@ -48,7 +49,7 @@ public class Serenity_2_0 {
                     TreeMap<Date, Date> TREE_MAP = new TreeMap<>();
                     Map<Long, NormalCandle> CE_MAP = new HashMap<>();
                     Map<Long, NormalCandle> PE_MAP = new HashMap<>();
-                    Date date = prepareData(file,TREE_MAP,CE_MAP,PE_MAP);
+                    Date date = prepareData(file, TREE_MAP, CE_MAP, PE_MAP);
 
                     List<NormalCandle> ceNormalCandles = new ArrayList<>();
                     List<NormalCandle> peNormalCandles = new ArrayList<>();
@@ -57,17 +58,20 @@ public class Serenity_2_0 {
 
 
                     for (Map.Entry<Date, Date> longLongEntry : TREE_MAP.entrySet()) {
-                          ceNormalCandles.add(CE_MAP.get(longLongEntry.getKey().getTime()));
+                        ceNormalCandles.add(CE_MAP.get(longLongEntry.getKey().getTime()));
                         peNormalCandles.add(PE_MAP.get(longLongEntry.getKey().getTime()));
-                        boolean exited = serenityAlgo.executeTrades(peNormalCandles,ceNormalCandles);
-                        if(exited){
+                        boolean exited = serenityAlgo.executeTrades(peNormalCandles, ceNormalCandles);
+                        if (exited) {
                             break;
                         }
                     }
 
-                    DATE_PROFIT_MAP.put(date,serenityAlgo.profitLoss);
+                    if(serenityAlgo.shortStraddle){
+                        throw new RuntimeException("trades still open");
+                    }
+                    DATE_PROFIT_MAP.put(date, serenityAlgo.profitLoss);
                     STRING_LIST_MAP.put(date, serenityAlgo.tradeLogs);
-                    System.out.println("finished for date "+date);
+                    System.out.println("finished for date " + date);
                 }
             });
             futures.add(future);
@@ -120,7 +124,7 @@ public class Serenity_2_0 {
         }
         printWriter.flush();
         System.out.println("_____________________________________________________________");
-        System.out.println(" time taken in seconds "+(new Date().getTime() - start.getTime())/1000);
+        System.out.println(" time taken in seconds " + (new Date().getTime() - start.getTime()) / 1000);
         Thread.sleep(1000);
         System.exit(0);
     }
@@ -128,67 +132,66 @@ public class Serenity_2_0 {
 
     @SneakyThrows
     private static Date prepareData(File file, Map TREE_MAP, Map CE_MAP, Map PE_MAP) {
-            SimpleDateFormat SIMPLE_DATE_FORMAT_1 = new SimpleDateFormat("dd/MM/yy");
-            SimpleDateFormat SIMPLE_DATE_FORMAT_2 = new SimpleDateFormat("dd.MM.yy");
-            SimpleDateFormat SIMPLE_DATE_FORMAT_3 = new SimpleDateFormat("dd.MM.yy HH:mm:ss");
-            String s = null;
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        SimpleDateFormat SIMPLE_DATE_FORMAT_1 = new SimpleDateFormat("dd/MM/yy");
+        SimpleDateFormat SIMPLE_DATE_FORMAT_2 = new SimpleDateFormat("dd.MM.yy");
+        SimpleDateFormat SIMPLE_DATE_FORMAT_3 = new SimpleDateFormat("dd.MM.yy HH:mm:ss");
+        String s = null;
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
 
-            Date date1 = null;
-            String callOrPut = null;
+        Date date1 = null;
+        String callOrPut = null;
 
-            while ((s = bufferedReader.readLine()) != null) {
-                //System.out.println(s);
-                if (StringUtils.isBlank(s)) {
-                    continue;
-                }
-                if (date1 == null) {
-                    date1 = SIMPLE_DATE_FORMAT_1.parse(s.replaceAll("-", "/"));
-                    continue;
-                }
-                if (s.contains("CE") || s.contains("PE")) {
-                    callOrPut = s;
-                    continue;
-                }
-
-                if (s.contains(SIMPLE_DATE_FORMAT_2.format(date1))) {
-                    String[] strings = s.split(",");
-                    Date date = SIMPLE_DATE_FORMAT_3.parse(strings[0]);
-                    /**
-                     * 28.12.21 12:23:59,378.90,378.90,364.00,370.00,31375,932800,361.07
-                     * date      time    o h l c xx,xx vwap(atp)
-                     */
-                    Float open = Float.parseFloat(strings[1]);
-                    Float high = Float.parseFloat(strings[2]);
-                    Float low = Float.parseFloat(strings[3]);
-                    Float close = Float.parseFloat(strings[4]);
-                    Float atp = Float.parseFloat(strings[7]);
-                    long volume = Long.parseLong(strings[5]);
-
-                    NormalCandle normalCandle = NormalCandle.builder()
-                            .open(open).high(high).close(close).low(low).volume(volume).date(date).symbol(callOrPut)
-                            .build();
-
-                    if (normalCandle.getSymbol().contains("CE")) {
-                        CE_MAP.put(date.getTime(), normalCandle);
-                    }
-                    if (normalCandle.getSymbol().contains("PE")) {
-                        PE_MAP.put(date.getTime(), normalCandle);
-                    }
-
-                    TREE_MAP.put(date, date);
-
-                }
+        while ((s = bufferedReader.readLine()) != null) {
+            //System.out.println(s);
+            if (StringUtils.isBlank(s)) {
+                continue;
             }
-            String s1 = "";
-            return date1;
+            if (date1 == null) {
+                date1 = SIMPLE_DATE_FORMAT_1.parse(s.replaceAll("-", "/"));
+                continue;
+            }
+            if (s.contains("CE") || s.contains("PE")) {
+                callOrPut = s;
+                continue;
+            }
+
+            if (s.contains(SIMPLE_DATE_FORMAT_2.format(date1))) {
+                String[] strings = s.split(",");
+                Date date = SIMPLE_DATE_FORMAT_3.parse(strings[0]);
+                /**
+                 * 28.12.21 12:23:59,378.90,378.90,364.00,370.00,31375,932800,361.07
+                 * date      time    o h l c xx,xx vwap(atp)
+                 */
+                Float open = Float.parseFloat(strings[1]);
+                Float high = Float.parseFloat(strings[2]);
+                Float low = Float.parseFloat(strings[3]);
+                Float close = Float.parseFloat(strings[4]);
+                Float atp = Float.parseFloat(strings[7]);
+                long volume = Long.parseLong(strings[5]);
+
+                NormalCandle normalCandle = NormalCandle.builder()
+                        .open(open).high(high).close(close).low(low).volume(volume).date(date).symbol(callOrPut)
+                        .build();
+
+                if (normalCandle.getSymbol().contains("CE")) {
+                    CE_MAP.put(date.getTime(), normalCandle);
+                }
+                if (normalCandle.getSymbol().contains("PE")) {
+                    PE_MAP.put(date.getTime(), normalCandle);
+                }
+
+                TREE_MAP.put(date, date);
+
+            }
+        }
+        String s1 = "";
+        return date1;
     }
 
 
+    public static class SerenityAlgo {
 
-    public static class SerenityAlgo{
-
-        private static final int quantityPerLot= 25;
+        private static final int quantityPerLot = 25;
 
         private boolean shortStraddle = false;
         private float shortStraddleEntryPrice = 0;
@@ -206,7 +209,7 @@ public class Serenity_2_0 {
                 float low = normalCandleListCE.get(x).getLow() + normalCandleListPE.get(x).getHigh();
 
                 long volume = normalCandleListCE.get(x).getVolume() + normalCandleListPE.get(x).getVolume();
-                closeVolumeList.add(new CloseVolume(close, (close+high+low)/3, volume, normalCandleListCE.get(x).getDate()));
+                closeVolumeList.add(new CloseVolume(close, (close + high + low) / 3, volume, normalCandleListCE.get(x).getDate()));
                 strikeCE = normalCandleListCE.get(x).getSymbol();
                 strikePE = normalCandleListPE.get(x).getSymbol();
             }
@@ -221,40 +224,40 @@ public class Serenity_2_0 {
             }
             float vwap = priceVolumeSum / volumeSum;
 
-            if (!shortStraddle && closeVolumeList.get(closeVolumeList.size() - 1).getClose() < vwap && (closeVolumeList.size() <2 || closeVolumeList.get(closeVolumeList.size() - 2).getClose() > vwap)) {
-                if(closeVolumeList.get(closeVolumeList.size()-1).date.getHours() >= 15 && closeVolumeList.get(closeVolumeList.size()-1).date.getMinutes()>=20){
-                    return true;
-                }else {
-                    shortStraddleEntryPrice = closeVolumeList.get(closeVolumeList.size() - 1).getClose();
-                    shortStraddle = true;
-                    trades.addAndGet(4);
-                    tradeLogs.add("Shorting straddle at " + closeVolumeList.get(closeVolumeList.size() - 1).date + " at price " + shortStraddleEntryPrice + " for strike price " + strikeCE + "/" + strikePE);
+            if (!shortStraddle) {
+                if (closeVolumeList.get(closeVolumeList.size() - 1).getClose() < vwap && (closeVolumeList.size() < 2 || closeVolumeList.get(closeVolumeList.size() - 2).getClose() > vwap)) {
+                    if (closeVolumeList.get(closeVolumeList.size() - 1).date.getHours() >= 15 && closeVolumeList.get(closeVolumeList.size() - 1).date.getMinutes() >= 15) {
+                        return true;
+                    } else {
+                        shortStraddleEntryPrice = closeVolumeList.get(closeVolumeList.size() - 1).getClose();
+                        shortStraddle = true;
+                        trades.addAndGet(4);
+                        tradeLogs.add("Shorting straddle at " + closeVolumeList.get(closeVolumeList.size() - 1).date + " at price " + shortStraddleEntryPrice + " for strike price " + strikeCE + "/" + strikePE);
+                    }
                 }
-            }
-
-            if(shortStraddle) {
+            } else {
                 if (closeVolumeList.get(closeVolumeList.size() - 1).getClose() > vwap * 1.1) {
                     //stoploss of 10% hit
                     profitLoss = quantityPerLot * (shortStraddleEntryPrice - closeVolumeList.get(closeVolumeList.size() - 1).getClose());
-                    tradeLogs.add("Exiting Shorting straddle at "+closeVolumeList.get(closeVolumeList.size() - 1).date +" at price "+ closeVolumeList.get(closeVolumeList.size() - 1).getClose());
+                    tradeLogs.add("Exiting Shorting straddle at " + closeVolumeList.get(closeVolumeList.size() - 1).date + " at price " + closeVolumeList.get(closeVolumeList.size() - 1).getClose()+" at P/L "+profitLoss);
                     shortStraddle = false;
                     return false;
-                }else if(closeVolumeList.get(closeVolumeList.size() - 1).getClose() < vwap * 0.8){
+                } else if (closeVolumeList.get(closeVolumeList.size() - 1).getClose() < vwap * 0.7) {
                     //target of 30% hit
                     profitLoss = quantityPerLot * (shortStraddleEntryPrice - closeVolumeList.get(closeVolumeList.size() - 1).getClose());
-                    tradeLogs.add("Exiting Shorting straddle at "+closeVolumeList.get(closeVolumeList.size() - 1).date +" at price "+ closeVolumeList.get(closeVolumeList.size() - 1).getClose());
+                    tradeLogs.add("Exiting Shorting straddle at " + closeVolumeList.get(closeVolumeList.size() - 1).date + " at price " + closeVolumeList.get(closeVolumeList.size() - 1).getClose()+" at P/L "+profitLoss);
                     shortStraddle = false;
                     return false;
-                }else if(closeVolumeList.get(closeVolumeList.size()-1).date.getHours() >= 15 && closeVolumeList.get(closeVolumeList.size()-1).date.getMinutes()>=20){
-                    //exit at 15:20
+                } else if (closeVolumeList.get(closeVolumeList.size() - 1).date.getHours() >= 15 && closeVolumeList.get(closeVolumeList.size() - 1).date.getMinutes() >= 15) {
+                    //exit at 15:15
                     profitLoss = quantityPerLot * (shortStraddleEntryPrice - closeVolumeList.get(closeVolumeList.size() - 1).getClose());
-                    tradeLogs.add("Exiting Shorting straddle at "+closeVolumeList.get(closeVolumeList.size() - 1).date +" at price "+ closeVolumeList.get(closeVolumeList.size() - 1).getClose());
+                    tradeLogs.add("Exiting Shorting straddle at " + closeVolumeList.get(closeVolumeList.size() - 1).date + " at price " + closeVolumeList.get(closeVolumeList.size() - 1).getClose()+" at P/L "+profitLoss);
                     shortStraddle = false;
                     return true;
                 }
             }
 
-            if(closeVolumeList.get(closeVolumeList.size()-1).date.getHours() >= 15 && closeVolumeList.get(closeVolumeList.size()-1).date.getMinutes()>=20){
+            if (closeVolumeList.get(closeVolumeList.size() - 1).date.getHours() >= 15 && closeVolumeList.get(closeVolumeList.size() - 1).date.getMinutes() >= 20) {
                 return true;
             }
             return false;
@@ -264,11 +267,11 @@ public class Serenity_2_0 {
 
     @Getter
     public static class CloseVolume {
-        private float price,close;
+        private float price, close;
         private long volume;
         private Date date;
 
-        public CloseVolume(float close,float price, long volume, Date date) {
+        public CloseVolume(float close, float price, long volume, Date date) {
             this.price = price;
             this.close = close;
             this.volume = volume;
